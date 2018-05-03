7f13bfa12946e9aca9a61e85fa2a04a6219c9175
Zoe <zoereagan@gmail.com>
2018-05-03 11:03:38 -0500

    url params

--- app.rb
+++ app.rb
@@ -6,5 +6,21 @@ Bundler.require()
 #whenever you add a new gem, you BUNDLE!!
 
 get '/' do
+  #like res.send in Express
+  #remember the last line of any method is what is will return
   'This is a new server! Great job u coder u!'
+end
+
+#define a route-- DO NOT HAVE A TRAILING '/'
+get '/hello' do
+   "hello World"
+end
+
+#URL parameters
+get '/hello/:some_param' do
+  #URL parameters are store in params Hash
+
+  the_param = params['some_param']
+  "hello #{the_param}"
+
 end
